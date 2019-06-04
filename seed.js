8e98f58a3fb535c348196c6f39b133ab81aff422
Paige Gorry <paigeegorry@gmail.com>
2019-06-03 19:14:40 -0700

    added default paging to all characters and basic queries route

--- seed.js
+++ seed.js
@@ -5,10 +5,12 @@ const Character = require('./lib/models/Character');
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 
-module.exports = () => {
+const seedData = () => {
   return fetchCharacterInfo()
     .then(characters => Character.create(characters))
     .then(() => console.log('done'))
     .finally(() => mongoose.connection.close())
     .catch(console.log);
 };
+
+seedData();
